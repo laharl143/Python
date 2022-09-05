ad7e0f467349430522622b0f4fed004928b851a8
unknown <erskine.duenas@gmail.com>
2022-09-05 14:08:24 +0800

    added some notes

--- notes.py
+++ notes.py
@@ -1,3 +1,5 @@
+from distutils.log import info
+from doctest import script_from_examples
 from msilib.schema import File
 
 
@@ -21,12 +23,12 @@ Use the community!
 
 The Python Bible “How to ask an
 Excellent Question” Checklist
-Hi There! Use the following checklist to ask an excellent question in the course forums when you
-have a problem related to python programming. Either myself or someone else will be there to help
-you as quick as a flash!
+Hi There! Use the following checklist to ask an excellent question in the course forums when you have a problem related to python programming. Either myself or someone else will be there to help you as quick as a flash!
+
 1) Make a great question title ☐
 a. I have added the [Help] Tag at the start of my title ☐
 b. My Question title is Clear and descriptive and it is obvious what the problem is ☐
+
 2) My Question has a complete description ☐
 a. I have written down the observed behaviour (What has happened) ☐
 b. I have written down the expected behaviour (What was supposed to happen) ☐
@@ -34,7 +36,46 @@ c. I have added a link to my code using gist.github.com ☐
 d. I have mentioned what I think the problem might be and the location. ☐
 e. I have provided a list of at least 3 things that I have tried to solve the problem ☐
 f. My Description uses full sentences with correct grammar and punctuation ☐
+
 3) I have included other relevant information
 a. Error messages in full ☐
 b. Version of python used ☐
 c. Screen shots where necessary ☐ 
+
+
+
+How to ask Question in a Community Example:
+    
+Title: 
+    
+    [Help] Answer not being shown on screen when running Python script 
+
+Description: 
+    
+    1) Observed Behaviour - 
+    Script runs successfully but answer doesnt show up
+    
+    2) Expected Behaviour - 
+    I expected the answer 2 to show up when I ran the script.
+    
+    3) Link to Code: #(Use gist.github) - 
+    https://gist.github.com/laharl143/2b3072c1ebc8245a182800355fcc3955
+    
+    4) Where I think the problem is-
+    There must be something different between script and the shell
+    It worked in the shell...lines 1-2 hold the issue
+    Is something missing?
+    
+    5) What I have tried -
+    I tried running the script with different numbers. No difference.
+    
+    6) Errors and Warnings 
+    None
+    
+    7) Version info
+    3.5.2
+    
+    8) Screenshots
+    N/A
+
+
