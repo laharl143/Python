a1561ec7a801f075fd81e7f52970a19c214a75fb
unknown <erskine.duenas@gmail.com>
2022-09-05 13:47:56 +0800

    added some notes

--- notes.py
+++ notes.py
@@ -6,3 +6,35 @@ Open IDLE
 Open new File
 
 F5 to run and it will open in IDLE
+
+
+
+
+
+
+
+#The secrets of a Top Programmers:
+How to ask great questions
+
+Use the community!
+
+
+The Python Bible “How to ask an
+Excellent Question” Checklist
+Hi There! Use the following checklist to ask an excellent question in the course forums when you
+have a problem related to python programming. Either myself or someone else will be there to help
+you as quick as a flash!
+1) Make a great question title ☐
+a. I have added the [Help] Tag at the start of my title ☐
+b. My Question title is Clear and descriptive and it is obvious what the problem is ☐
+2) My Question has a complete description ☐
+a. I have written down the observed behaviour (What has happened) ☐
+b. I have written down the expected behaviour (What was supposed to happen) ☐
+c. I have added a link to my code using gist.github.com ☐
+d. I have mentioned what I think the problem might be and the location. ☐
+e. I have provided a list of at least 3 things that I have tried to solve the problem ☐
+f. My Description uses full sentences with correct grammar and punctuation ☐
+3) I have included other relevant information
+a. Error messages in full ☐
+b. Version of python used ☐
+c. Screen shots where necessary ☐ 
