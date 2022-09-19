6c1261b02834a5174e1e36b452e30d8b89220fef
unknown <erskine.duenas@gmail.com>
2022-09-19 18:29:25 +0800

    update

---
+++ Projects/hello_you/helllo_you.py
@@ -0,0 +1,25 @@
+#Ask user for name
+name = input("What is your name?: ")
+
+
+#Ask user for their age
+age = input("What is your age?: ")
+
+
+#Ask user for city
+city = input("Where do you live?: ")
+
+#Ask user what they enjoy
+hobby = input("What do you enjoy the most?: ")
+
+print(name)
+print(age)
+print(city)
+print(hobby)
+#Create output text
+
+
+#Print output to screen
+
+
+
