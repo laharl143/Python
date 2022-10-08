23372dc720f31b5d15758bd9bb735aaa2012ec3a
unknown <erskine.duenas@gmail.com>
2022-10-09 06:45:14 +0800

    update

---
+++ Practice Python with 100 Python Exercises/Exercises/7.py
@@ -0,0 +1,7 @@
+# Question: List slicing is important in various data manipulation activities. Let's do a few more exercises on that.
+
+# Please complete the script so that it prints out the first three items of list letters.
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+print(letters[:3])  #['a', 'b', 'c']
---
+++ Practice Python with 100 Python Exercises/Exercises/8.py
@@ -0,0 +1,7 @@
+# Question: Complete the script so that it prints out the letter i  using negative indexing.
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+# print(letters[8:7:-1])  #my solution
+
+print(letters[-2])  #best solution
