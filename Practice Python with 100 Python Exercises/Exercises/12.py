2667ca846f74b6fc806a5c358e7b66fbb8157e2b
unknown <erskine.duenas@gmail.com>
2022-10-09 07:01:22 +0800

    update

---
+++ Practice Python with 100 Python Exercises/Exercises/12.py
@@ -0,0 +1,9 @@
+# Question: Complete the script so that it produces the expected output. Please use my_range  as input data.
+
+#  Expected output: 
+
+# [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200] 
+
+
+my_range = range(10, 201, 10)   #(start,stop,step)
+print(list(my_range))   #[10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200]
