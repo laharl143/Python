fd2f157809a39dba2296fb07ec0408b0d997b183
unknown <erskine.duenas@gmail.com>
2022-10-09 08:23:04 +0800

    update

---
+++ Practice Python with 100 Python Exercises/Exercises/18.py
@@ -0,0 +1,12 @@
+# Question. What is wrong in this script? And how do you fix it to print out 'Smith'
+
+d = {"Name": "John", "Surname": "Smith"}
+# print(d["Smith"])
+
+# solution: 
+print(d["Surname"])     #Smith 
+
+#explanation: just call out the dictionary
+
+
+#did not solved this
--- Practice Python with 100 Python Exercises/Exercises/tempCodeRunnerFile.py
+++ Practice Python with 100 Python Exercises/Exercises/tempCodeRunnerFile.py
@@ -1,2 +1,1 @@
-d = {"a": 1, "b": 2}
-# print(d)    #d = {"a": 1, "b": 2}
+print(d["Smith"])
